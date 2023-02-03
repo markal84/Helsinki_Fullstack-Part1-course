e34d92207e80d3f7bf6da7d0f1a1828fdeac399c
Marcin K <49732790+markal84@users.noreply.github.com>
2023-02-03 08:49:20 +0100

    Update App.js
    fix: remove unnecessary comments

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,47 +1,5 @@
 import { useState } from 'react';
 
-/*
-function History({ allClicks }) {
-  if (allClicks.length === 0) {
-    return <div>the app is used by pressing buttons</div>;
-  }
-  return <div>button press history: {allClicks.join('| ')}</div>;
-}
-
-function Button({ handleClick, text }) {
-  return (
-    <button type="button" onClick={handleClick}>
-      {text}
-    </button>
-  );
-}
-
-function App() {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'));
-    setLeft(left + 1);
-  };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'));
-    setRight(right + 1);
-  };
-
-  return (
-    <div>
-      {left}
-      <Button handleClick={handleLeftClick} text="left" />
-      <Button handleClick={handleRightClick} text="right" />
-      {right}
-      <History allClicks={allClicks} />
-    </div>
-  );
-}
-*/
 
 function Feedback({ good, neutral, bad }) {
   return (
